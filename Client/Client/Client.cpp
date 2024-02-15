4d7f97445799d48cd5d4b93b5eabed5e31d1b7d2
jongeon <eonesty@naver.com>
2024-02-15 18:55:34 -0500

    First commit. add skeleton template

---
+++ Client/Client/Client.cpp
@@ -0,0 +1,18 @@
+/*
+* Filename: Client.cpp
+* Project: Lab_W6B
+* By: Jongeon Lee, Honggyu Park
+* Date: Feb 15, 2024
+* Description: Make Server and Client with syslog login feature.
+*/
+
+#include <iostream>
+#include <WS2tcpip.h>
+
+#pragma comment(lib, "ws2_32.lib")
+
+int main() {
+
+
+    return 0;
+}
---
+++ Server/Server/Server.cpp
@@ -0,0 +1,17 @@
+/*
+* Filename: Server.cpp
+* Project: Lab_W6B
+* By: Jongeon Lee, Honggyu Park
+* Date: Feb 16, 2024
+* Description:
+*/
+
+#include <iostream>
+#include <WS2tcpip.h>
+
+#pragma comment(lib, "Ws2_32.lib")
+
+int main() {
+
+	return 0;
+}
