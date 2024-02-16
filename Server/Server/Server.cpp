07530e001958b0adc0fad6615a8ea532b020d31b
chris <117550785+inventor1004@users.noreply.github.com>
2024-02-15 19:56:13 -0500

    Declare varibles which are need TCP connection.

--- Server/Server/Server.cpp
+++ Server/Server/Server.cpp
@@ -5,13 +5,20 @@
 * Date: Feb 16, 2024
 * Description:
 */
-
 #include <iostream>
+#include <WinSock2.h>
 #include <WS2tcpip.h>
 
 #pragma comment(lib, "Ws2_32.lib")
 
 int main() {
-
-	return 0;
+    
+    WSADATA wsaData;            // WSADATS: Window Socket API DATA
+    int server_fd;              // Server_File_Discriptor: Server side socket identifier
+    int new_socket;             // Identify the new socket connection (Use for client connection)
+    int valread;                // Number of read bytes will be stored
+    struct sockaddr_in address; // The information for the internet connection(IP Address, Port Number) will be stored
+    int addrlen = sizeof(address); 
+    int opt = 1;                // Represents Socket Option
+    char buffer[1024] = { 0 };
 }
