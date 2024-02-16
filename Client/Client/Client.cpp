db4a72374e1b6efc401f5d1d58a87bd90c4ae932
jongeon <eonesty@naver.com>
2024-02-15 19:09:09 -0500

    added Winsock initialisation and Sock code

--- Client/Client/Client.cpp
+++ Client/Client/Client.cpp
@@ -12,7 +12,35 @@
 #pragma comment(lib, "ws2_32.lib")
 
 int main() {
+    // Winsock initilation
+    WSADATA wsData;
+    WORD ver = MAKEWORD(2, 2);
+
+    int wsResult = WSAStartup(ver, &wsData);
+    if (wsResult != 0) {
+        std::cerr << "Winsock initialization failed. error code: " << wsResult << std::endl;
+        return -1;
+    }
+
+    // Create socket
+    SOCKET sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock == INVALID_SOCKET) {
+        std::cerr << "Socket creation failed. error code: " << WSAGetLastError() << std::endl;
+        WSACleanup();
+        return -2;
+    }
 
 
+
+
+
+
+
+    // Socket Shutdown
+    closesocket(sock);
+
+    // Winsock shutdown
+    WSACleanup();
+    
     return 0;
 }
